969dc52d327648a954ad9f781175c909cee475f4
Tim Mitra <tim@it-guy.com>
2023-11-27 15:05:39 -0500

    how to add user and append jobs. Also add optional delete rule

--- SwiftDataProject/User.swift
+++ SwiftDataProject/User.swift
@@ -13,7 +13,11 @@ final class User {
   var name: String
   var city: String
   var joinDate: Date
-  var jobs = [Job]()
+  // var jobs = [Job]() // can be done w/o Relationship macro
+  // deleteRule
+  // nullify deletes and leaves jobs
+  // cascade will delete jobs
+  @Relationship(deleteRule: .cascade) var jobs = [Job]()
     
   init(name: String, city: String, joinDate: Date) {
     self.name = name
--- SwiftDataProject/UsersView.swift
+++ SwiftDataProject/UsersView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct UsersView: View {
+  @Environment(\.modelContext) var modelContext
   @Query var users: [User]
   
   var body: some View {
@@ -27,6 +28,8 @@ struct UsersView: View {
           .clipShape(.capsule )
       }
     }
+    // for fun.
+    //.onAppear(perform: addSample)
   }
   
   init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
@@ -34,6 +37,17 @@ struct UsersView: View {
     _users = Query(filter: #Predicate<User> { user in
       user.joinDate >= minimumJoinDate
     }, sort: sortOrder)
+  }
+  
+  func addSample() {
+    let user1 = User(name: "Piper Chapman", city: "New York", joinDate: .now)
+    let job1 = Job(name: "Organize sock drawer", priority: 3)
+    let job2 = Job(name: "Make plans with Alex", priority: 4)
+    // insert w/o jobs
+    modelContext.insert(user1)
+    // append the jobs to the user1
+    user1.jobs.append(job1)
+    user1.jobs.append(job2)
   }
 }
 
