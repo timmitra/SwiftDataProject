9bc5076ef4932be1d3900a260abd0de95ae3d7e2
Tim Mitra <tim@it-guy.com>
2023-11-27 13:05:13 -0500

    using the dynamic filter and sort, with Picker and tag, then wrapped in a menu. (removed the Edit and Add buttons to follow Paul's code)

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -10,30 +10,17 @@ import SwiftData
 
 struct ContentView: View {
   @Environment(\.modelContext) private var modelContext
-  // using predicate macro
-  @Query(filter: #Predicate<User>{ user in
-    user.name.localizedStandardContains("R") &&
-    user.city == "London"
-  }, sort: \User.name) var users: [User]
-  @State private var path = [User]()
-
+  @State private var showingUpcomingOnly = false
+  @State private var sortOrder = [
+    SortDescriptor(\User.name),
+    SortDescriptor(\User.joinDate)
+  ]
+  
   var body: some View {
-    NavigationStack(path: $path) {
-      List(users) { user in
-        NavigationLink(value: user) {
-          Text(user.name)
-        }
-      }
+    NavigationStack {
+      UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
       .navigationTitle("Users")
-      .navigationDestination(for: User.self) { user in
-        EditUserView(user: user)
-      }
       .toolbar {
-        Button("Add User", systemImage: "plus") {
-          let user = User(name: "", city: "", joinDate: .now)
-          modelContext.insert(user)
-          path = [user]
-        }
         Button("Add Samples", systemImage: "plus.square.on.square") {
           // clear out data
           try? modelContext.delete(model: User.self)
@@ -46,6 +33,23 @@ struct ContentView: View {
           modelContext.insert(second)
           modelContext.insert(third)
           modelContext.insert(fourth)
+        }
+        Button(showingUpcomingOnly ? "Show Everyone" : "Show upcoming") {
+          showingUpcomingOnly.toggle()
+        }
+        Menu("Sort", systemImage: "arrow.up.arrow.down") {
+          Picker("Sort", selection: $sortOrder) {
+            Text("Sort by Name")
+              .tag([
+                SortDescriptor(\User.name),
+                SortDescriptor(\User.joinDate)
+              ])
+            Text("Sort by Join Date")
+              .tag([
+                SortDescriptor(\User.joinDate),
+                SortDescriptor(\User.name)
+              ])
+          }
         }
       }
     }
